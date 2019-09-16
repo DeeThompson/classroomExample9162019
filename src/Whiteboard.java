caae102e3f01d9d38c8ae7640058501c9fd18d27
deethompson <devon.thompson0@gmail.com>
2019-09-16 13:25:45 -0400

    java class work

---
+++ src/Whiteboard.java
@@ -0,0 +1,18 @@
+public class Whiteboard {
+
+    private String title;
+    private String author;
+    private String genre;
+    private int numberOfPages;
+    private int numberOfChapters;
+    private String publisher;
+
+    //whiteboard of getters
+    public String getGenre (){
+        return genre;
+    }
+    public void setTitle (String title) {
+        this.title = title
+
+    }
+}
